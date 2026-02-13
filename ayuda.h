ab067d020e0bb8649d038b11b868b0e2cb2bb442
Ivan140809 <ivansantiagolastra1408@gmail.com>
2026-02-13 10:42:10 -0500

    Add the contente of help

---
+++ ayuda.h
@@ -0,0 +1,12 @@
+#ifndef Ayuda_H
+#define Ayuda_H
+
+#include <string>
+using namespace std;
+// Muestra lista de comandos disponibles
+void ayuda_print_general();
+
+// Muestra ayuda de uso para un comando específico (ayuda comando)
+void ayuda_print_for(string& cmd);
+
+#endif
